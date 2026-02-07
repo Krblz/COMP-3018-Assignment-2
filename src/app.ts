8c7e5d72758b1f9e7ad44ad2f0a9d3396572f586
Keith <krobles3@rrc.ca>
2026-02-06 22:46:42 -0600

    Updated Apps so it is connected to Ticket Routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express, { Express } from "express";
+import ticketRoutes from "./api/v1/routes/ticketRoutes";
 
 // Initialize Express application
 const app: Express = express();
+
+app.use(express.json());
+app.use("/api/v1", ticketRoutes);
 
 // Define a route
 app.get("/", (req, res) => {
